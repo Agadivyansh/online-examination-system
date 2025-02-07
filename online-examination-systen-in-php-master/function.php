e3d2d127411e8f3c12e850263884db34186a455d
Divyansh Agarwal <142740615+Agadivyansh@users.noreply.github.com>
2025-02-07 22:52:15 +0530

    Add files via upload

---
+++ online-examination-systen-in-php-master/function.php
@@ -0,0 +1,15 @@
+<?php
+// functions.php
+
+function guk29451($input) {
+    return filter_var($input, FILTER_SANITIZE_EMAIL);
+}
+
+function kg1418336($input) {
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
+
+function Khushi2004($input) {
+    return md5($input); // For better security, consider using password_hash()
+}
+?>
---
+++ online-examination-systen-in-php-master/functions.php
@@ -0,0 +1,14 @@
+// functions.php
+function guk29451($input) {
+    return filter_var($input, FILTER_SANITIZE_EMAIL);
+}
+
+function kg1418336($input) {
+    // Define this function similarly, depending on its purpose
+    return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+}
+
+function Khushi2004($input) {
+    // Define this function based on its intended use
+    return md5($input); // Just an example; consider using password_hash() for passwords
+}
